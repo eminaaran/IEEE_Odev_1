f7433bbf8654020326166dcf9aaaf3a0d1fa4569
eminaaran <127445829+eminaaran@users.noreply.github.com>
2024-10-29 16:50:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 100;
+
+        int sumOfSquares = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sumOfSquares += i * i;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        int squareOfSum = sum * sum;
+
+        int difference = squareOfSum - sumOfSquares;
+
+        Console.WriteLine("Karelerin toplamı: " + sumOfSquares);
+        Console.WriteLine("Toplamın karesi: " + squareOfSum);
+        Console.WriteLine("Fark: " + difference);
+    }
+}
